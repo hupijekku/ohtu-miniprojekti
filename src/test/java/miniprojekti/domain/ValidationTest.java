64b37519a3394dc6a58e985fdf46a506b2c35224
ktatu <kaikkonentatu@gmail.com>
2020-12-07 18:43:43 +0200

    Kaikkien lukuvinkkityyppien validointi-testit

--- src/test/java/miniprojekti/domain/ValidationTest.java
+++ src/test/java/miniprojekti/domain/ValidationTest.java
@@ -3,9 +3,7 @@ package miniprojekti.domain;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,24 +11,12 @@ public class ValidationTest {
     
     private Validation testVal = new Validation();
     
-    ArrayList<String> errors;
+    private ArrayList<String> errors;
     
-    Map<String, String> bookParams;
-    Map<String, String> podcastParams;
-    Map<String, String> videoParams;
-    Map<String, String> blogpostParams;
-    
-    
-    public ValidationTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    private Map<String, String> bookParams;
+    private Map<String, String> podcastParams;
+    private Map<String, String> videoParams;
+    private Map<String, String> blogpostParams;
     
     @Before
     public void setUp() {
@@ -39,6 +25,23 @@ public class ValidationTest {
                 "note", "A must-read for all programmers",
                 "author", "Thomas David, Hunt Andrew", "isbn", "9780201616224",
                 "url", "https://www.amazon.com/Pragmatic-Programmer-journey-mastery-Anniversary-ebook/dp/B07VRS84D1"));
+        
+        blogpostParams = new HashMap<>(Map.of("title", "InversionOfControl", 
+                "type", "Blogpost",
+                "note", "A very easy to understand explanation",
+                "url", "https://martinfowler.com/bliki/InversionOfControl.html"));
+        
+        podcastParams = new HashMap<>(Map.of("title", "Developer Tea", 
+                "type", "Podcast",
+                "note", "Over 5 years of content",
+                "author", "Jonathan Cutrell",
+                "description", "Wide range of topics. Relatively short episodes",
+                "url", "https://spec.fm/podcasts/developer-tea"));
+        
+        videoParams = new HashMap<>(Map.of("title", "Clean Code - Uncle Bob / Lesson 1",
+                "type", "Video",
+                "note", "Series contains 6 lessons",
+                "url", "https://www.youtube.com/watch?v=7EmboKQH8lM&t"));
         
     }
     
@@ -80,6 +83,7 @@ public class ValidationTest {
         testForMessage("'title' must be 3-50 characters");
     }
     
+    // BookTip-validoinnit
     @Test
     public void bookWithPassableValuesGivesNoErrors() {
         errors = testVal.validate(bookParams);
@@ -102,6 +106,57 @@ public class ValidationTest {
     public void bookWithIncorrectlyFormattedISBNGivesAnError() {
         updateParamAndValidate(bookParams, "isbn", "12345678");
         testForMessage("does not contain a valid ISBN number");
+    }
+    
+    @Test
+    public void bookDoesNotRequireUrl() {
+        updateParamAndValidate(bookParams, "url", "");
+        assertEquals(0, errors.size());
+    }
+    
+    // BlogpostTip-validoinnit
+    @Test
+    public void blogpostWithPassableValuesGivesNoErrors() {
+        errors = testVal.validate(blogpostParams);
+        assertEquals(0, errors.size());
+    }
+    
+    @Test
+    public void blogpostWithoutUrlGivesAnError() {
+        updateParamAndValidate(blogpostParams, "url", "");
+        testForMessage("'url' must be filled");
+    }
+    
+    @Test
+    public void blogpostWithIncorrectlyFormattedUrlGivesAnError() {
+        updateParamAndValidate(blogpostParams, "url", "ftp://downloadthisfile.com");
+        testForMessage("does not contain a valid url");
+    }
+    
+    // PodcastTip-validoinnit
+    @Test
+    public void podcastWithPassableValuesGivesNoErrors() {
+        errors = testVal.validate(podcastParams);
+        assertEquals(0, errors.size());
+    }
+    
+    @Test
+    public void podcastWithoutUrlGivesAnError() {
+        updateParamAndValidate(podcastParams, "url", "");
+        testForMessage("'url' must be filled");
+    }
+    
+    // VideoTip-validoinnit
+    @Test
+    public void videoWithPassableValuesGivesNoErrors() {
+        errors = testVal.validate(videoParams);
+        assertEquals(0, errors.size());
+    }
+    
+    @Test
+    public void videoWithoutUrlGivesAnError() {
+        updateParamAndValidate(videoParams, "url", "");
+        testForMessage("'url' must be filled");
     }
     
     public void updateParamAndValidate(Map<String, String> paramMap, String fieldName, String newValue) {
